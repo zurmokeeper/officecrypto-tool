ee55f5f2e2f2a9ba17ef4ae567a60bd10e52fb3f
linxl <1658370535@qq.com>
2024-07-02 20:11:01 +0800

    feat: update

--- tests/rc4/encrypt.test.js
+++ tests/rc4/encrypt.test.js
@@ -8,14 +8,14 @@ const filePath = './tests/data/encrypt';
 
 describe('xls rc4 encrypt', () => {
   it('encrypt success', async () => {
-    const input = await fs.readFile(`${filePath}/rc4_pass_test.xls`);
+    const input = await fs.readFile(`${filePath}/rc4_wait_for_encrypt_test.xls`);
     const output = officeCrypto.encrypt(input, {password: '123456', type: 'rc4'});
     await fs.writeFile(`${filePath}/rc4_pass_out_success.xls`, output);
     // expect(200).toEqual(200);
   });
 
   it('encrypt success, input is ArrayBuffer', async () => {
-    const input = await fs.readFile(`${filePath}/rc4_pass_test.xls`);
+    const input = await fs.readFile(`${filePath}/rc4_wait_for_encrypt_test.xls`);
     const arrayBuffer = input.buffer.slice(input.byteOffset, input.byteOffset + input.byteLength);
     const output = officeCrypto.encrypt(arrayBuffer, {password: '123456', type: 'rc4'});
     await fs.writeFile(`${filePath}/rc4_pass_out_success_arraybuffer.xls`, output);
@@ -23,7 +23,7 @@ describe('xls rc4 encrypt', () => {
   });
 
   it('encrypt success, input is TypeBuffer', async () => {
-    const input = await fs.readFile(`${filePath}/rc4_pass_test.xls`);
+    const input = await fs.readFile(`${filePath}/rc4_wait_for_encrypt_test.xls`);
     const typeBuffer = new Uint8Array(input);
     const output = officeCrypto.encrypt(typeBuffer, {password: '123456', type: 'rc4'});
     await fs.writeFile(`${filePath}/rc4_pass_out_success_typeBuffer.xls`, output);
@@ -32,7 +32,7 @@ describe('xls rc4 encrypt', () => {
 
 
   it('encrypt success', async () => {
-    const input = await fs.readFile(`${filePath}/rc4_pass_test.xls`);
+    const input = await fs.readFile(`${filePath}/rc4_wait_for_encrypt_test.xls`);
     const password = '1234abcd'.repeat(31) + '1234567';
     const output = officeCrypto.encrypt(input, {password, type: 'rc4'});
     await fs.writeFile(`${filePath}/rc4_pass_out_success_255.xls`, output);
--- tests/rc4_crypto_api/encrypt.test.js
+++ tests/rc4_crypto_api/encrypt.test.js
@@ -8,34 +8,34 @@ const filePath = './tests/data/encrypt';
 
 describe('xls rc4_crypto_api encrypt', () => {
   it('encrypt success', async () => {
-    const input = await fs.readFile(`${filePath}/rc4_crypto_api_pass_test.xls`);
+    const input = await fs.readFile(`${filePath}/rc4_crypto_api_wait_for_encrypt_test.xls`);
     const output = officeCrypto.encrypt(input, {password: '123456', type: 'rc4_crypto_api'});
-    await fs.writeFile(`${filePath}/rc4_pass_out_success.xls`, output);
+    await fs.writeFile(`${filePath}/rc4_crypto_api_pass_out_success.xls`, output);
     // expect(200).toEqual(200);
   });
 
   it('encrypt success, input is ArrayBuffer', async () => {
-    const input = await fs.readFile(`${filePath}/rc4_crypto_api_pass_test.xls`);
+    const input = await fs.readFile(`${filePath}/rc4_crypto_api_wait_for_encrypt_test.xls`);
     const arrayBuffer = input.buffer.slice(input.byteOffset, input.byteOffset + input.byteLength);
     const output = officeCrypto.encrypt(arrayBuffer, {password: '123456', type: 'rc4_crypto_api'});
-    await fs.writeFile(`${filePath}/rc4_pass_out_success_arraybuffer.xls`, output);
+    await fs.writeFile(`${filePath}/rc4_crypto_api_pass_out_success_arraybuffer.xls`, output);
     // expect(200).toEqual(200);
   });
 
   it('encrypt success, input is TypeBuffer', async () => {
-    const input = await fs.readFile(`${filePath}/rc4_crypto_api_pass_test.xls`);
+    const input = await fs.readFile(`${filePath}/rc4_crypto_api_wait_for_encrypt_test.xls`);
     const typeBuffer = new Uint8Array(input);
     const output = officeCrypto.encrypt(typeBuffer, {password: '123456', type: 'rc4_crypto_api'});
-    await fs.writeFile(`${filePath}/rc4_pass_out_success_typeBuffer.xls`, output);
+    await fs.writeFile(`${filePath}/rc4_crypto_api_pass_out_success_typeBuffer.xls`, output);
     // expect(200).toEqual(200);
   });
 
 
   it('encrypt success', async () => {
-    const input = await fs.readFile(`${filePath}/rc4_crypto_api_pass_test.xls`);
+    const input = await fs.readFile(`${filePath}/rc4_crypto_api_wait_for_encrypt_test.xls`);
     const password = '1234abcd'.repeat(31) + '1234567';
     const output = officeCrypto.encrypt(input, {password, type: 'rc4_crypto_api'});
-    await fs.writeFile(`${filePath}/rc4_pass_out_success_255.xls`, output);
+    await fs.writeFile(`${filePath}/rc4_crypto_api_pass_out_success_255.xls`, output);
     // expect(200).toEqual(200);
   });
 });
